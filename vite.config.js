fcf8c459f1ecfbd0f3eec499c61cbe26db84ac4f
Jsequeirag <joseluissequeirag2@gmail.com>
2024-01-02 20:48:40 -0600

    hello world

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,54 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
-const manifestForPlugIn = {
-  registerType: "prompt",
-  includeAssests: ["favicon.ico", "apple-touch-icon.png", "masked-icon.png"],
-  manifest: {
-    name: "React-vite-app",
-    short_name: "react-vite-app",
-    description: "I am a simple vite app",
-    icons: [
-      {
-        src: "/android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "favicon",
-      },
-      {
-        src: "/android-chrome-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "favicon",
-      },
-      {
-        src: "/apple-touch-icon.png",
-        sizes: "180x180",
-        type: "image/png",
-        purpose: "apple touch icon",
-      },
-      {
-        src: "/maskable_icon.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any maskable",
-      },
-    ],
-    theme_color: "#171717",
-    background_color: "#f0e7db",
-    display: "standalone",
-    scope: "/",
-    start_url: "/",
-    orientation: "portrait",
-  },
-};
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA(manifestForPlugIn)],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        // caches the assets/icons mentioned (assets/* includes all the assets present in your src/ directory)
+        includeAssets: ["favicon.ico", "apple-touch-icon.png", "assets/*"],
+        name: "Uber direct",
+        short_name: "PWA Guide",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x51.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/icon-1024x1024.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/maskable-512x512.png",
+            type: "image/png",
+            sizes: "512x512",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
